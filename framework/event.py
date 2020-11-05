8583b1eee399901b329a0de53a71be7390a2ca06
Marius Montebaur <marius@montebaur.tech>
2020-11-05 21:07:13 +0100

    async event plugin POC successfull

--- framework/event.py
+++ framework/event.py
@@ -18,6 +18,8 @@ Requirements:
 
 import asyncio
 import time
+import inspect
+
 
 
 class EventPluginBase:
@@ -28,18 +30,24 @@ class EventPluginBase:
         self.update_event = asyncio.Event()
         self.next_return_time = None
         self.message_queue = []
+        self.message_handlers = {}
 
-    def subclassable_event_loop(self, msg):
-        # TODO: fix ugly name
-        """ To be overridden by the user. """
-        print("Now handling a message with user-defined code:", msg)
-        time.sleep(1.5)
+    def initialize(self):
+        """ To be overridden by subclasses to do any necessary
+        initialization. """
+        pass
 
-    async def initialize(self):
-        """ Initialize function to override. """
-        rate = 1/5  # Hz
+    async def event_loop(self):
+        """
+        May be overridden by subclasses if custom periodic tasks should be handled
+        by the plugin, e.g. reading an external sensor every 10 seconds. Example usage:
+        ```
+        rate = 1/10  # 0.1 Hz = executed every 10 seconds
         while await self.spin_once(rate):
-            print("~~ periodic important task ~~")
+            print("Important periodic task here.")
+        ```
+        """
+        await self.spin()
 
     async def event_wait(self, evt, timeout):
         # wait for: ( event_triggered  or  time_up )
@@ -49,15 +57,37 @@ class EventPluginBase:
             pass
         return evt.is_set()
 
-    async def spin_once(self, rate):
+    def _ramining_time(self, rate):
+        if rate is None:
+            return None
+        t = time.time()
 
         if self.next_return_time is None:
-            self.next_return_time = time.time()
-            return True
-        self.next_return_time = max(self.next_return_time + 1/rate, time.time())
+            self.next_return_time = t
+
+        if t > self.next_return_time:
+            self.next_return_time = max(self.next_return_time + 1/rate, t)
+
+        return self.next_return_time - t
+
+    async def spin_once(self, rate):
+        """
+        Plugin's task handling. Will try to return on time so that code can
+        be executed with a frequency given by `rate`. Returns `False` if the
+        plugin received a signal to shut down. `spin_once(..)` will not do any
+        computations itself if all topic callbacks are asynchronous
+        functions.
+        """
+        # TODO: Deal with rate = None for no timeout
+
+        # if self.next_return_time is None:
+        #     self.next_return_time = time.time()
+        #     return True
+        # self.next_return_time = max(self.next_return_time + 1/rate, time.time())
 
         while True:
-            sleep_time = max(0, self.next_return_time - time.time())
+            # sleep_time = max(0, self.next_return_time - time.time())
+            sleep_time = self._ramining_time(rate)
             by_event = await self.event_wait(self.update_event, sleep_time)
 
             if not by_event:
@@ -69,10 +99,40 @@ class EventPluginBase:
                 print("--> plugin {}.{} exiting".format(__name__, self.__class__.__name__))
                 return False
 
+            # Execute callback for longest waiting message
             msg = self.message_queue.pop()
-            self.subclassable_event_loop(msg)
+            topic, payload = msg["topic"], msg.get("payload", None)
+            callback = self.message_handlers.get(topic, None)
+            if callback is None:
+                print("No callback available for message in topic {}".format(topic))
+            elif inspect.iscoroutinefunction(callback):
+                # launch asynchronously and return immediately
+                asyncio.ensure_future(callback(payload))
+            else:
+                callback(payload)
+
             print("Done with msg handling.")
 
+    async def spin(self):
+        """
+        Will not return until the plugin receives the command to shut down.
+        It's important that `await self.spin()` is called in the event loop
+        as it enables the plugin to handle messages with its defined
+        callbacks.
+        """
+        await self.spin_once(None)
+
+    def register_topic_callback(self, topic_name, callback):
+        """
+        Registers a callback function that will be called once the plugin
+        receives a message on the given topic. `callback` may be a
+        synchronous or asynchronous function (a coroutine defined with async
+        def ...).
+        Topic name 'websocket' is reserved for messages that are received by
+        the server over a websocket connection.
+        """
+        # TODO: use register_topic_callback for websocket messages as well.
+        self.message_handlers[topic_name] = callback
 
     def receive_message(self, msg):
         self.message_queue.append(msg)
@@ -81,33 +141,4 @@ class EventPluginBase:
     def shutdown(self):
         self._should_shutdown = True
         self.update_event.set()
-
-
-class EventPlugin(EventPluginBase):
-    pass
-
-
-async def main():
-
-    ep = EventPlugin()
-
-    async def sender():
-        await asyncio.sleep(7)
-        ep.receive_message("hello world")
-        print("placed msg")
-        await asyncio.sleep(16)
-        print("sender done")
-
-    ep_task = asyncio.create_task(ep.initialize())
-
-    await sender()
-    await asyncio.sleep(10)
-
-    ep.shutdown()
-
-    await ep_task
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
 
---
+++ tests/plugin_tests.py
@@ -0,0 +1,110 @@
+#
+# plugin_tests.py
+# backyardbot
+#
+# Created: November 2020
+# Author: Marius Montebaur
+# montebaur.tech, github.com/montioo
+#
+
+import time
+from framework.event import EventPluginBase
+import asyncio
+import unittest
+
+
+start_time = time.time()
+og_print = print
+print_list = []
+
+def time_print(*args, **kwargs):
+    global start_time, print_list
+    delta_time = time.time() - start_time
+    # og_print(round(delta_time, 2), *args, **kwargs)
+    print_list.append((round(delta_time, 2), args[0]))
+
+print = time_print
+
+
+class EventPluginToTest(EventPluginBase):
+
+    def initialize(self):
+        self.register_topic_callback("example_topic", self.message_callback)
+
+    async def message_callback(self, msg):
+        """ To be overridden by the user. """
+        print("+++ start callback for msg {}".format(msg))
+        await asyncio.sleep(5)
+        print("--- done with callback")
+
+    async def event_loop(self):
+        rate = 1/5  # 0.2 Hz = executed every 5 seconds
+        while await self.spin_once(rate):
+            print("~~ important periodic task here. ~~")
+
+
+async def main():
+
+    ep = EventPluginToTest()
+    ep.initialize()
+
+    async def sender():
+        await asyncio.sleep(7)
+        ep.receive_message({"topic": "example_topic", "payload": "hello world"})
+        print("placed msg")
+        await asyncio.sleep(6)
+        print("sender done")
+
+    ep_task = asyncio.create_task(ep.event_loop())
+
+    await sender()
+    await asyncio.sleep(5)
+
+    print("sending shutdown signal to plugin")
+    ep.shutdown()
+
+    await ep_task
+
+
+class TestPluginEventSystem(unittest.TestCase):
+
+    def setUp(self):
+        global print_list
+        print_list = []
+        self.ep = EventPluginToTest()
+        self.ep.initialize()
+
+    def tearDown(self):
+        pass
+
+    def test_async_spin_and_msg(self):
+        """
+        Will initialize a plugin, set an asynchronous callback for messages
+        and check that the execution timing for both event loop and message
+        callback work as expected.
+        """
+        asyncio.run(main())
+        ref = [
+            (0.0, "~~ important periodic task here. ~~"),
+            (5.01, "~~ important periodic task here. ~~"),
+            (7.01, "placed msg"),
+            # (None, "Done with msg handling."),
+            (7.01, "+++ start callback for msg hello world"),
+            (10.01, "~~ important periodic task here. ~~"),
+            (12.01, "--- done with callback"),
+            (13.01, "sender done"),
+            (15.0, "~~ important periodic task here. ~~"),
+            (18.01, "sending shutdown signal to plugin"),
+            # (None, "--> plugin framework.event.EventPluginToTest exiting")
+        ]
+        global print_list
+        times_ref, str_ref = zip(*ref)
+        times_out, str_out = zip(*print_list)
+        self.assertEqual(list(str_ref), list(str_out))
+        # almostEqual for collection types only available in np
+        for time_ref, time_out in zip(times_ref, times_out):
+            self.assertAlmostEqual(time_ref, time_out, places=1)
+
+
+if __name__ == "__main__":
+    unittest.main()
